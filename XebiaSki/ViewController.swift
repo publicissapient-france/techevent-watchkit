848211499b0082615fd7f78c6446cc8f10fc5172
Simone Civetta <simonecivetta@gmail.com>
2014-12-03 10:47:18 +0100

    Initial Commit

---
+++ XebiaSki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XebiaSki
+//
+//  Created by Simone Civetta on 03/12/14.
+//  Copyright (c) 2014 Xebia IT Architechts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
