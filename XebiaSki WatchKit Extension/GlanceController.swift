825e0cddf661ce4af27c0fee8fb57ea085fed893
Simone Civetta <simonecivetta@gmail.com>
2014-12-03 15:22:53 +0100

    Revert "Add Glance screen"
    This reverts commit 2369c6b354c90358deba054ce1ed98b4c5cafec6.

--- XebiaSki WatchKit Extension/GlanceController.swift
+++ XebiaSki WatchKit Extension/GlanceController.swift
@@ -2,49 +2,28 @@
 //  GlanceController.swift
 //  XebiaSki WatchKit Extension
 //
-//  Created by Simone Civetta on 19/11/14.
+//  Created by Simone Civetta on 03/12/14.
 //  Copyright (c) 2014 Xebia IT Architechts. All rights reserved.
 //
 
 import WatchKit
 import Foundation
-import XebiaSkiFramework
+
 
 class GlanceController: WKInterfaceController {
 
-    @IBOutlet weak var mainGroup: WKInterfaceGroup!
-    @IBOutlet weak var titleLabel: WKInterfaceLabel!
-    
-    private var photoDownloadManager: PhotoDownloadManager?
-    
-    var selectedSkiResort: SkiResort? {
-        get {
-            if let data = NSUserDefaults.standardUserDefaults().valueForKey("selection")? as? NSData {
-                return NSKeyedUnarchiver.unarchiveObjectWithData(data) as? SkiResort
-            }
-            return nil
-        }
-    }
-    
     override init(context: AnyObject?) {
+        // Initialize variables here.
         super.init(context: context)
+        
+        // Configure interface objects here.
+        NSLog("%@ init", self)
     }
 
     override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        
-        if let skiResort = self.selectedSkiResort? {
-            self.photoDownloadManager = PhotoDownloadManager(photoURL: skiResort.photoURL)
-            self.photoDownloadManager?.retrievePhoto({ [weak self] (image) -> () in
-                if let weakSelf = self? {
-                    weakSelf.mainGroup.setBackgroundImage(image)
-                }
-            })
-            self.titleLabel.setText(skiResort.name)
-        } else {
-            self.mainGroup.setBackgroundImage(nil)
-            self.titleLabel.setText("")
-        }
+        NSLog("%@ will activate", self)
     }
 
     override func didDeactivate() {
