961c4aeabf2280fd06799eeff9663f9b05490d82
Simone Civetta <simonecivetta@gmail.com>
2014-12-03 11:00:06 +0100

    Add dynamic framework

---
+++ XebiaSkiFramework/XebiaSkiFramework.h
@@ -0,0 +1,19 @@
+//
+//  XebiaSkiFramework.h
+//  XebiaSkiFramework
+//
+//  Created by Simone Civetta on 03/12/14.
+//  Copyright (c) 2014 Xebia IT Architechts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XebiaSkiFramework.
+FOUNDATION_EXPORT double XebiaSkiFrameworkVersionNumber;
+
+//! Project version string for XebiaSkiFramework.
+FOUNDATION_EXPORT const unsigned char XebiaSkiFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XebiaSkiFramework/PublicHeader.h>
+
+
